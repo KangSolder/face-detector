f31abd5f24422853ac8c7c635eb81e5d150015e7
Leak27 <oki271002@gmail.com>
2021-12-04 10:08:53 +0800

    add credit

---
+++ face-detector.py
@@ -0,0 +1,20 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (top, right, bottom, left) in faces:
+        cv2.rectangle(frame,(top,right),(top+bottom,right+left),(0,0,255),2)
+
+    cv2.imshow('Face Detection',frame)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
